c8d89395aa5a17da082479bae40ec206052d2193
ayushma-gupta04 <72247893+ayushma-gupta04@users.noreply.github.com>
2021-03-11 22:02:29 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -28,14 +28,14 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>Bot Uptime ⌚:</b> {currentTime}\n' \
+    stats = f'<b>Bot Uptime ⏳:</b> {currentTime}\n' \
             f'<b>Total disk space🗄️:</b> {total}\n' \
-            f'<b>Used 🗃️:</b> {used}  ' \
-            f'<b>Free 🗃️:</b> {free}\n\n' \
+            f'<b>Used 🗂️:</b> {used}  ' \
+            f'<b>Free 📂:</b> {free}\n\n' \
             f'📇Data Usage📇\n<b>Uploaded :</b> {sent}\n' \
             f'<b>Downloaded:</b> {recv}\n\n' \
             f'<b>CPU 🖥️:</b> {cpuUsage}% ' \
-            f'<b>RAM ⛏️:</b> {memory}% ' \
+            f'<b>RAM ⚙️:</b> {memory}% ' \
             f'<b>Disk 🗄️:</b> {disk}%'
     sendMessage(stats, context.bot, update)
 
